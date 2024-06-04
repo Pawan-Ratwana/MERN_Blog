afdf000ac1802b9f1cb7fb726a7b0597dbe83712
Pawan Kumar <ratwanapawan@gmail.com>
2024-06-04 10:29:31 +0530

    create pages and routes for client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/Sign-in';
+import SignUp from './pages/Sign-up';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects'
+
 function App() {
+ 
 
   return (
-    <div className='text-xl'>Jai Shree Ram</div>
+   <BrowserRouter >
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/dashboard' element={ < Dashboard /> } />
+        <Route path='/projects' element={ <Projects />} />
+      </Routes>
+   </BrowserRouter>
   )
 }
 
-export default App;
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='text-xl text-red-500'>
+     Jai Shree Ram from Home
+    </div>
+  )
+}
+
+
+export default Home;
