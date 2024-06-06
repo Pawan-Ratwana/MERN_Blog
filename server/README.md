3d485d32be70a1369563090af9127cb0ffab06ec
Pawan Kumar <ratwanapawan@gmail.com>
2024-06-06 11:22:28 +0530

    connect to Database

---
+++ server/README.md
@@ -0,0 +1,4 @@
+1. npm init
+2. npm i express nodemon
+3. npm i mongoose
+4. npm i dotenv
--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 import express from "express";
 const app = express();
 const port = 3000;
+import mongoose from "mongoose";
+import env from 'dotenv';
+env.config();
+
+
+const uri = process.env.BLOG_MONGODB_URI;
+mongoose.connect(uri).then(() => {
+    console.log("MognoDB is connected successfully")
+}).catch((err) => {
+    console.log("Error to connect with MongoDB", err)
+});
 
 
 
